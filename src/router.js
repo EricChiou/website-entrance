2155c9747d01ee6e45a8bf22be0e98f09fe4207d
Chiu <Eric.Chiu@acereb.com>
2019-11-29 15:18:22 +0800

    update router path

--- src/router.js
+++ src/router.js
@@ -9,13 +9,13 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '*',
-      redirect: { name: 'home' }
-    },
-    {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '*',
+      redirect: { name: 'home' }
     }
   ]
 });
